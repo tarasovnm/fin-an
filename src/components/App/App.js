1c845ce2b600025e25a69286022d9bfd161434ea
tarasovnm <tarasovnm@gmail.com>
2020-12-13 19:40:22 +0400

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      Приложение для финансового анализа деятельности предприятия
+    </div>
+  );
+}
+
+export default App;
